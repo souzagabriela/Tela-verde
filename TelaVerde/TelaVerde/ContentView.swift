3cc94fd1cf20773be07c9e35ba53413a402f489b
souzagabriela <ellabaptist@outlook.com>
2022-07-12 11:36:07 -0300

    feat(ContentView): setup TabView

--- TelaVerde/TelaVerde/ContentView.swift
+++ TelaVerde/TelaVerde/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, developer branch!")
-            .padding()
+        VStack {
+            Text("Hello, developer branch!")
+                .padding()
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("Tab Content 1").tabItem {
+                    // image assets here
+                    /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+                Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            }
+        }
     }
 }
 
