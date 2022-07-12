b39f3848265828161ea66d1a7d854e4f0d5e97d3
souzagabriela <ellabaptist@outlook.com>
2022-07-12 10:52:21 -0300

    inital commit setup project

---
+++ TelaVerde/TelaVerde/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TelaVerde
+//
+//  Created by Gabriela Souza Batista on 12/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TelaVerde/TelaVerde/TelaVerdeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelaVerdeApp.swift
+//  TelaVerde
+//
+//  Created by Gabriela Souza Batista on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TelaVerdeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
