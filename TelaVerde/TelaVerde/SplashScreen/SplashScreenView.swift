1af3714ed52b6f92a758f9d9fb936c1103b27bae
souzagabriela <ellabaptist@outlook.com>
2022-07-12 21:20:18 -0300

    feat: splash screen view

---
+++ TelaVerde/TelaVerde/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,44 @@
+//
+//  SplashScreenView.swift
+//  TelaVerde
+//
+//  Created by Gabriela Souza Batista on 12/07/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    
+    var body: some View {
+        
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Image("SplashScreen")
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
